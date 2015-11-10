10e91f87ddce9c7489e7c7135a85e8c4c3b2b5ed
spiritpig <tceh528@126.com>
2015-11-10 19:32:21 +0800

    2015/11/10-Skill-EquipmentWnd

--- githubPush.sh
+++ githubPush.sh
@@ -6,25 +6,25 @@
 #请用其他分隔符如-
 #
 
-git add -a ProjectSettings
-git add -a Assets/Resources/Font
-git add -a Assets/Resources/Font.meta
-git add -a Assets/Resources/Mesh
-git add -a Assets/Resources/Mesh.meta
-git add -a Assets/Resources/Effect
-git add -a Assets/Resources/Effect.meta
-git add -a Assets/Resources/Material
-git add -a Assets/Resources/Material.meta
-git add -a Assets/Resources/Prefab
-git add -a Assets/Resources/Prefab.meta
-git add -a Assets/Resources/Sprite
-git add -a Assets/Resources/Sprite.meta
-git add -a Assets/Resources/order.txt
-git add -a Assets/Resources/order.txt.meta
-git add -a Assets/Scene
-git add -a Assets/Scene.meta
-git add -a Assets/Script
-git add -a Assets/Script.meta
+git add -A ProjectSettings
+git add -A Assets/Resources/Font
+git add -A Assets/Resources/Font.meta
+git add -A Assets/Resources/Mesh
+git add -A Assets/Resources/Mesh.meta
+git add -A Assets/Resources/Effect
+git add -A Assets/Resources/Effect.meta
+git add -A Assets/Resources/Material
+git add -A Assets/Resources/Material.meta
+git add -A Assets/Resources/Prefab
+git add -A Assets/Resources/Prefab.meta
+git add -A Assets/Resources/Sprite
+git add -A Assets/Resources/Sprite.meta
+git add -A Assets/Resources/order.txt
+git add -A Assets/Resources/order.txt.meta
+git add -A Assets/Scene
+git add -A Assets/Scene.meta
+git add -A Assets/Script
+git add -A Assets/Script.meta
 git add "Assets/New Terrain 1.asset"
 git add "Assets/New Terrain 1.asset.meta"
 git commit -am "$@"
